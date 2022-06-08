7732a03489079cdb83d18ed4f72cdbf8d5e9b697
Philip Huebner <info@philhuebner.com>
2022-06-08 18:45:21 -0500

    Update app.py
    set default context size to 1

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ st.sidebar.write('More information can be found at http://github.com/phueb/Missi
 context_sizes = [1, 2, 3, 4, 5, 6]
 context_size = st.sidebar.selectbox('Select the context size.',
                                     context_sizes,
-                                    index=1)
+                                    index=0)
 
 num_dims_select = list(range(1, 16))
 num_dims = st.sidebar.selectbox('Select the number of singular dimensions to keep.',
